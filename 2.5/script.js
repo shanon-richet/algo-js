24a79da2fc395101810d006077272da9efc382c6
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2021-09-10 09:44:59 +0200

    Add files via upload

---
+++ 2.5/script.js
@@ -0,0 +1,27 @@
+for (let i = 1; i <= 100; i += 1) {
+    console.log(i);
+  }
+
+  let num = prompt("What's your favourite number?");
+  let i = 42;
+
+  while(true){
+  if (num != i){
+    (console.log);
+    alert('Are you sure?');
+   num = prompt("What's your favourite number?"); 
+  }
+  else{
+    alert('Yes, is the best !')
+  }
+}
+
+
+
+
+
+
+
+
+
+
