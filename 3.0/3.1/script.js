83db96b32e71e27fd43816fefa9165f06c1507e2
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2021-09-10 15:57:21 +0200

    Add files via upload
    Exercices with arrays

---
+++ 3.0/3.1/script.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 3, 4, 5];
+
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+console.log(arr.reduce(reducer));
+       
---
+++ 3.0/3/script.js
@@ -0,0 +1,15 @@
+const arr = ["Kelian Christophe",
+    "Kévin Monsieur",
+    "Martin Neyns",
+    "Léon FASHINGABO", 
+    "Mohammad Azad",
+    "Hanna Masanjika",
+    "Victor Tardif",
+    "Veraart Esteban",
+    "Youssef Akanni",
+    "Shanon Richet",
+    "Dylan Richardson",
+    "Franquinet Aubry"];
+for (const element of arr) {
+    console.log(element);
+}
