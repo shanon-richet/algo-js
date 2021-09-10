b660bda7f83492a827244bbe349280e4021a753a
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2021-09-10 16:11:20 +0200

    Update script.js

--- 3.0/3.1/script.js
+++ 3.0/3.1/script.js
@@ -1,6 +1,7 @@
 const arr = [1, 2, 3, 4, 5];
-
 const reducer = (previousValue, currentValue) => previousValue + currentValue;
+console.log(arr.reduce(reducer));
+
+arr.push(100, 101, 102);
 
 console.log(arr.reduce(reducer));
-       
