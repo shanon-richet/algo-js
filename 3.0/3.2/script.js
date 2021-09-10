f955fe2052ec9a42e3a83e8eb992957f3cd57155
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2021-09-10 16:58:52 +0200

    Add files via upload
    Calcul d'une moyenne

---
+++ 3.0/3.2/script.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4, 5];
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+console.log(arr.reduce(reducer)/ arr.length);
+
+const array = (100, 101, 102);
+
+console.log(arr.reduce(reducer)/ array.length);
+
+
