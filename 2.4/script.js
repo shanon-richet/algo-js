caf2dee00fbeeabaed31e0779c51d8f8eecca67b
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2021-09-30 10:02:46 +0200

    Update script.js

--- 2.4/script.js
+++ 2.4/script.js
@@ -1,13 +1,9 @@
 for (let i = 1; i <= 100; i += 1) {
   console.log(i);
-  if (i %2 === 0){
-    console.log(i / 2);
-  }
   if (i % 2 === 1){
-    console.log(i * 3)
+    console.log(i)
   }
 }
-
 
 
 
